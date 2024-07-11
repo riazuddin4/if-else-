60d98e401117be7d9de403c7029397326414934f
Riaz Uddin <mdriaj2010bd@gmail.com>
2024-07-12 01:00:32 +0600

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,9 @@
+applePrice = 210
+
+budget =  int(input("Enter your budget:"))
+
+
+if (applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
